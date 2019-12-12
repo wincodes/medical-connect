677c7c3a6476f0ae9028c920b96c9aa4740e69b8
wincodes <otokinaodafe@gmail.com>
2019-12-12 15:52:23 +0100

    separate the frontend and use dotenv for config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://wincodes:mp9e9qvGmy8h37Qk@cluster0-0ogoh.mongodb.net/medical_connect?retryWrites=true&w=majority",
-  secret: 'secret'
+  mongoURI: process.env.mongoURI,
+  secret: process.env.APP_SECRET
 };
--- server.js
+++ server.js
@@ -1,38 +1,53 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 
-const app = express();
+const app = express()
+
+//allow requests from cross origin
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+	)
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+	)
+	next()
+})
 
 //body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport)
 
 //db config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI
 
 //connect to db
 mongoose
-  .connect(db)
-  .then(() => console.log("mongodb connected"))
-  .catch(err => console.log(err));
+	.connect(db)
+	.then(() => console.log('mongodb connected'))
+	.catch(err => console.log(err))
 
 //add passport middleware
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 //routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`server started at port ${port}`));
+app.listen(port, () => console.log(`server started at port ${port}`))
